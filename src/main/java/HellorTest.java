f8f235518245b72dfb1b72334a00213aa1cf9ed3
Cihat Eminsoy <cihateminsoy@gmail.com>
2021-01-31 01:56:00 +0800

    initial commit from intelliJ updated.

--- src/main/java/HellorTest.java
+++ src/main/java/HellorTest.java
@@ -3,5 +3,6 @@ public class HellorTest {
     public static void main(String[] args) {
 
         System.out.println("Hello World Test");
+        System.out.println("EU4 is doing great!");
     }
 }
